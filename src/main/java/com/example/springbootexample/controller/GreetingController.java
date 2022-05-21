4f7045bca23333d99cd8c317ae5512718f2fd900
AkshataHasabi <akshuh818@gmail.com>
2022-05-21 11:38:23 +0530

    ADDED:created controller return json for diffrent http method

--- src/main/java/com/example/springbootexample/controller/GreetingController.java
+++ src/main/java/com/example/springbootexample/controller/GreetingController.java
@@ -11,10 +11,36 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
+    private static final String template="Hello %s";
+    private static AtomicInteger counter=new AtomicInteger();
     @GetMapping("/hello")
     public String hello(){
         return "Hello From Bridgelabz";
     }
 
+    @GetMapping("/getmessage")
+    public Greeting getMessage(@RequestParam(value="name",defaultValue="World") Greeting greeting) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, greeting.getMessage()));
+    }
+
+    @PostMapping("/post")
+    public String sayHelloPost(@RequestBody Greeting greeting){
+        return "Hello"+" "+greeting.getMessage()+"!";
+    }
+
+    @PostMapping("/postMessage")
+    public ResponseEntity<String> getGreeting(@RequestBody Greeting greeting){
+        return new ResponseEntity<String>(greeting.getMessage(), HttpStatus.OK);
+    }
+
+    @PostMapping ("/postMessage1")
+    public Greeting addGreeting(@RequestBody Greeting greeting){
+        return new Greeting(counter.incrementAndGet(),String.format(template, greeting.getMessage(), HttpStatus.OK));
+    }
+
+    @PutMapping("/putMessage")
+    public Greeting editGreeting(@RequestBody Greeting greeting){
+        return new Greeting(counter.incrementAndGet(),String.format(template, greeting.getMessage(), HttpStatus.OK));
+    }
 
 }
